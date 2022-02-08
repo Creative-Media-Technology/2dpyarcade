bef6fec82369597709dad283daffa8e1a32ac8c1
shedtronic <38550478+shedtronic@users.noreply.github.com>
2022-02-08 16:20:06 +0000

    Added Alien and added direction keys
    altered testkeys.py to have directional input for testing on Raspberry Pi

--- testkeys.py
+++ testkeys.py
@@ -2,7 +2,7 @@
 
 ##Raspberry pi also has an Antimicro port
 ##Install using
-##   sudo apt-get -t buster-backports install antimicro
+##sudo apt-get -t buster-backports install antimicro
 
 ##Then you need to run either Qjoypad or Antimicro and configure your controller scheme for the controller you are using
 ##Then you can run this script to check that each of the controller bindings are working with this script before moving on to
@@ -16,10 +16,17 @@ HEIGHT = 600
 
 last_key_type = 0
 last_key_press = ""
+space_pressed = 1
+##Draw an alien
+alien = Actor('alien')
+alien.pos = 100, 56
+
 
 def draw():
     screen.clear()
     screen.draw.text("Last key press: "+last_key_press+" key: "+str(last_key_type), (20, 100))
+    alien.draw()
+
 
 def on_key_down(key, mod, unicode):
     global last_key_press, last_key_type
@@ -28,3 +35,13 @@ def on_key_down(key, mod, unicode):
         quit()
     last_key_type = key
     last_key_press = unicode# Write your code here :-)
+
+def update():
+    if keyboard.a:
+        alien.x += -5
+    if keyboard.d:
+        alien.x += 5
+    if keyboard.w:
+        alien.y += -5
+    if keyboard.s:
+        alien.y += 5
